3fac4d6309e43623924ed477e70556e03194846c
Jahidul Alam <mjahidulalam@gmail.com>
2021-02-06 12:10:25 -0700

    updated departments tab to drop on hover and added new search bar

--- forum/urls.py
+++ forum/urls.py
@@ -4,14 +4,11 @@ from . import views
 app_name = 'forum'
 
 urlpatterns = [
-    path('', views.home, name='forum-home'),
+    path('',views.home, name='forum-home'),
     path('departments/', views.departments, name='departments'),
     path('departments/<single_slug>', views.single_slug, name='single_slug'),
     path('about/', views.about, name='forum-about'),
     path('post/', views.post_list, name='forum_post_list'),
     path('new_post/', views.new_post, name='new_post'),
-    path('post/<int:year>/<int:month>/<int:day>/<slug:post>/',
-    
-         views.post_detail,
-         name='post_detail'),
+    path('post/<int:year>/<int:month>/<int:day>/<slug:post>/',views.post_detail,name='post_detail'),
 ]
